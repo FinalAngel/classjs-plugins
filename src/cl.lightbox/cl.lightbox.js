73a6ce52dba17c2db6471ae200bda35f78d75a73
Angelo Dini <finalangeljp@hotmail.com>
2012-12-19 14:52:06 +0100

    version update to 3 digits

--- src/cl.lightbox/cl.lightbox.js
+++ src/cl.lightbox/cl.lightbox.js
@@ -1,7 +1,7 @@
 /*!
  * @author      Angelo Dini - github.com/finalangel/classjs-plugins
  * @copyright	Distributed under the BSD License.
- * @version     1.0
+ * @version     1.0.0
  */
 
 // ensure namespace is defined
