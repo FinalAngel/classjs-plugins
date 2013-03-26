723352d3ab5e3c7ae4208f80309f128ed448ff77
Angelo Dini <finalangeljp@hotmail.com>
2013-03-26 23:49:21 +0100

    added js folder

---
+++ examples/static/js/base.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function ($) {
+
+	// iframe function
+	$('.iframe').on('click', function (e) {
+		e.preventDefault();
+
+		// calculate size
+		var size = ($(window).height() - $('header').height() - $('footer').height());
+		var style = 'width:100%; height:' + size + 'px; margin:0; border:none; margin:0 0 -5px;';
+
+		$('section').html('<iframe src="' + $(this).attr('href') + '" style="' + style + '" frameborder="0"></iframe>');
+
+		// add resizing
+		$(window).on('resize', function () {
+			var size = ($(window).height() - $('header').height() - $('footer').height());
+			$('iframe').css('height', size);
+		});
+
+		// remove scrollbars
+		$('body').css('overflow', 'hidden');
+	});
+});
