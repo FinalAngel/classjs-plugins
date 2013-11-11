e0f5fd26caad6f88002fddc91bb69cc10f81f8af
FinalAngel <angelo.dini@divio.ch>
2013-11-11 14:34:01 +0100

    added infos on grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,5 +76,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Default task.
-	grunt.registerTask('default', ['qunit', 'jshint', 'uglify']);
+	grunt.registerTask('default', ['uglify', 'qunit', 'jshint']);
 };
