0cb8c6105938994ec52b1c9f7474930e6be0ece2
FinalAngel <angelo.dini@divio.ch>
2014-06-25 08:06:01 +0200

    bump version

--- src/cl.autocomplete/CHANGELOG.rst
+++ src/cl.autocomplete/CHANGELOG.rst
@@ -2,6 +2,12 @@
 Cl.Autocomplete
 ===============
 
+1.1.0
+-----
+- added filtering options
+- refactor of validation
+- refactor of replacement
+
 1.0.0
 -----
 - initial release
--- src/cl.autocomplete/cl.autocomplete.js
+++ src/cl.autocomplete/cl.autocomplete.js
@@ -2,7 +2,7 @@
  * Cl.Autocomplete
  * @author      Ales Kocjancic - github.com/finalangel/classjs-plugins
  * @copyright   Distributed under the BSD License.
- * @version     1.0.0
+ * @version     1.1.0
  */
 
 // ensure namespace is defined
@@ -19,15 +19,27 @@ var Cl = window.Cl || {};
 			'minLength': 3,
 			'easing': 'swing',
 			'duration': 300,
-			'delay': 300,
+			'delay': 30,
 			'fx': 'slide',
-			'closeOnBlur': false,
+			'closeOnBlur': true,
+			'showEmpty' : false,
+			'dropdown': false,
 			'cls': {
 				'field': 'input[type="search"]',
+				'filter': 'input[name="filter"]',
 				'results': '.autocomplete-results',
+				'filtering': '.autocomplete-filtering',
 				'item': '.autocomplete-item',
-				'close': '.autocomplete-close',
-				'submit': '.autocomplete-submit'
+				'close': '.autocomplete-close, .close',
+				'focus': '.autocomplete-focus',
+				'submit': '.autocomplete-submit',
+				'icon': '.autocomplete-icon',
+                'highlightFieldOnClick': true,
+                'openAllOnClick': true,
+                'clearFieldOnClick': false
+			},
+			'attr': {
+				'value' : 'data-value'
 			},
 			'lang': {
 				'error': 'There has been an error!',
@@ -36,12 +48,12 @@ var Cl = window.Cl || {};
 		},
 
 		initialize: function(container, options) {
-			this.selector = container;
 			this.container = $(container);
 			this.options = $.extend(true, {}, this.options, options);
 			this.url = this.options.url ? this.options.url : this.container.attr('action');
 			this.results = this.container.find(this.options.cls.results);
 			this.field = this.container.find(this.options.cls.field);
+			this.fltr = this.container.find(this.options.cls.filter, this.container);
 			this.callbacks = {};
 
 			this.timer = function(){};
@@ -79,25 +91,74 @@ var Cl = window.Cl || {};
 
 			// stop propagation event on container so body can close
 			if (this.options.closeOnBlur) {
-				this.field.on('click', function (e) {
+				this.container.on('click', function (e) {
 					e.stopPropagation();
 				});
 				$(document.body).on('click', function () {
 					that.hide();
+					that._fire('close');
 				});
 			}
 
 			// bind extra form submit buttons
-			$(document).on('click', this.selector+' '+this.options.cls.submit, function(e) {
+			$(this.options.cls.submit, this.container).bind('click', function(e) {
 				e.preventDefault();
 				that.container.submit();
 			});
 
-			// bind close event
-			$(document).on('click', this.selector+' '+this.options.cls.close, function(e) {
-				e.preventDefault();
-				that.hide();
+			// setup dropdown
+			if (this.options.dropdown) {
+				this._typeDropdown();
+			}
+
+		},
+
+		_typeDropdown: function() {
+			var that = this;
+
+			// initial value
+			var initial = this.field.val();
+			this.field.attr(this.options.attr.value, initial);
+
+			// clear on click
+			this.field.on('click', function(e) {
+                if(that.options.cls.highlightFieldOnClick){ that.field.select(); }
+                if(that.options.cls.clearFieldOnClick){ that.field.val(''); }
+                if(that.options.cls.openAllOnClick){
+                    that.search(' ', true);
+                }else{
+                    that.search();
+                }
 			});
+
+			// toggle on icon click
+			$(this.options.cls.icon, this.container).bind('click', function(evt) {
+				evt.preventDefault();
+				evt.stopPropagation();
+				if (that._isVisible()) {
+					that.hide();
+					that._fire('close');
+				}
+				else {
+                    if(that.options.cls.clearFieldOnClick){ that.field.val(''); }
+                    if(that.options.cls.highlightFieldOnClick){ that.field.select(); }
+                    if(that.options.cls.openAllOnClick){
+                        that.search(' ', true);
+                    }else{
+                        that.search();
+                    }
+				}
+				that.field.focus();
+                if(!that.options.cls.highlightFieldOnClick){ that.field.blur(); }
+			});
+
+			// callback close
+			this.callbacks.close = function () {
+
+				// reset original value
+				var original = that.field.attr(that.options.attr.value);
+				that.field.val(original);
+			};
 		},
 
 		show: function() {
@@ -122,14 +183,16 @@ var Cl = window.Cl || {};
 			this._fire('hide');
 		},
 
-		search: function(query) {
+		search: function(query, showAll) {
 			var that = this;
 			query = query || this.field.val();
+            showAll = showAll || false;
 			if(this._validate(query)) {
 				that.query = query;
 				clearTimeout(that.timer);
+
 				that.timer = setTimeout(function() {
-					that._request();
+					that._request(showAll);
 				}, that.options.delay);
 			}
 
@@ -171,18 +234,34 @@ var Cl = window.Cl || {};
 
 		// validates query
 		_validate: function(query) {
+			// dropdown passes when different
+			if (this.options.dropdown) {
+				return (query !== this.field.attr(this.options.attr.value));
+			}
 			// passes if query is not empty, query is not open and has the correct length
-			return (query !== '' && query.length >= this.options.minLength && query !== this.query);
+			else {
+				return (query !== '' && query.length >= this.options.minLength && query !== this.query);
+			}
 		},
 
-		_request: function() {
+		_request: function(showAll) {
 			var that = this;
+            var data = "";
 
+            showAll = showAll || false;
+
+            if(showAll){
+                data = 'filter=&q=';
+            }else{
+                data = this.container.find('input').serialize();
+            }
+
+			// ajax request
 			$.ajax({
 				'type': 'GET',
 				'url': this.url,
 				'dataType': 'html',
-				'data': this.container.serialize(),
+				'data': data,
 				'success': function(data) {
 					if(data && data.length > 0) {
 						that._replace(data);
@@ -200,18 +279,91 @@ var Cl = window.Cl || {};
 		},
 
 		_replace: function(data) {
+			var that = this;
+
+			// result
 			this.results.html(data);
+
+			// handle item selection
+			$(this.options.cls.item, this.results).bind('click', function(evt) {
+				evt.preventDefault();
+
+				// set field value
+				var val = $(this).attr(that.options.attr.value);
+				if (val) {
+					that.field.val(val);
+					that.field.attr(that.options.attr.value,val);
+				}
+
+				// hide
+				that.hide();
+
+				// refocus
+				$(that.field).focus();
+
+				// notify
+				that._fire('selected');
+			});
+
+			// bind filter event
+			$(this.options.cls.filtering, this.results).bind('click', function(evt) {
+				evt.stopPropagation();
+				evt.preventDefault();
+
+				// set filter
+				var filter = $(this).attr('data-filter');
+				$(that.fltr).val(filter);
+
+				// refocus
+				$(that.field).focus();
+
+				// search
+				that.search();
+			});
+
+			// bind focus event
+			$(this.options.cls.focus, this.results).bind('click', function(evt) {
+				evt.preventDefault();
+
+				// refocus
+				$(that.field).focus();
+			});
+
+			// bind close event
+			$(this.options.cls.close, this.results).bind('click', function(evt) {
+				evt.preventDefault();
+
+				// hide
+				that.hide();
+
+				// refocus
+				$(that.field).focus();
+
+				// notify
+				that._fire('close');
+			});
+
+			// show
 			if (!this.results.is(':visible')) {
 				this.show();
 			}
 		},
 
 		_showEmpty: function() {
-			this._replace('<p class="autocomplete-message">' + this.options.lang.empty + '</p>');
+			if (this.options.showEmpty) {
+				this._replace('<p class="autocomplete-message">' + this.options.lang.empty + '</p>');
+			}
+			else {
+				this._replace('');
+			}
 		},
 
 		_showError: function() {
 			this._replace('<p class="autocomplete-message">' + this.options.lang.error +'</p>');
+		},
+
+		_isVisible: function() {
+			return (this.results.is(':visible'));
 		},
 
 		_fire: function (keyword) {
